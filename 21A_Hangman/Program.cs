11d7bb630d4e3e6963ad8d3a128b46a7791707aa
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-08-25 12:30:58 +0200

    Progress

--- 21A_Hangman/Program.cs
+++ 21A_Hangman/Program.cs
@@ -1,6 +1,6 @@
 ﻿// x Man bestämmer ett ord
 // x Man skriver ut så många streck som det finns bokstäver i ordet
-// - Man gissar en bokstav
+// x Man gissar en bokstav
 // - Kolla om det är rätt eller fel (finns med minst 1 gång i ordet)
 //   - Rätt: skriver ut bokstaven på rätt plats
 //   - Fel: Man blir av med 1 gissning, ritar ut en till del av gubben
@@ -10,25 +10,63 @@
 //   Eller om man fått slut på gissningar
 
 string word = "banana";
+int guesses = 7;
 
-List<string> underscores = new();
+List<string> underscores = MakeUnderscores(word);
 
-for (int i = 0; i < word.Length; i++)
+// while (string.Join("", underscores) != word)
+
+while (underscores.Contains("_") && guesses > 0)
 {
-  underscores.Add("_");
+  Console.WriteLine($"Du har {guesses} gissingar kvar");
+  Console.WriteLine(string.Join(" ", underscores));
+
+  // -- Hämta in en gissning
+  Console.WriteLine("GISSA");
+
+  string guess = GimmeGuess();
+
+  if (word.Contains(guess))
+  {
+    for (int i = 0; i < word.Length; i++)
+    {
+      if (word[i].ToString() == guess)
+      {
+        underscores[i] = guess;
+      }
+    }
+  }
+  else
+  {
+    guesses--;
+  }
+
 }
 
-Console.WriteLine(string.Join(" ", underscores));
-
-Console.WriteLine("GISSA");
-
-string guess = "";
-
-while (guess.Length != 1)
-{
-  guess = Console.ReadLine();
-}
-
-Console.WriteLine($"You guessed {guess}!");
+Console.WriteLine("GRATZ");
 
 Console.ReadLine();
+
+static List<string> MakeUnderscores(string word)
+{
+  List<string> underscores = new();
+
+  for (int i = 0; i < word.Length; i++)
+  {
+    underscores.Add("_");
+  }
+
+  return underscores;
+}
+
+static string GimmeGuess()
+{
+  string guess = "";
+
+  while (guess.Length != 1)
+  {
+    guess = Console.ReadLine();
+  }
+
+  return guess;
+}

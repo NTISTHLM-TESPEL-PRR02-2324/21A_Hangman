6bc0c1a7693c6447d48e43a7d393349f44f1e935
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-08-25 11:32:41 +0200

    Initial commit

---
+++ 21A_Hangman/Program.cs
@@ -0,0 +1,34 @@
+﻿// x Man bestämmer ett ord
+// x Man skriver ut så många streck som det finns bokstäver i ordet
+// - Man gissar en bokstav
+// - Kolla om det är rätt eller fel (finns med minst 1 gång i ordet)
+//   - Rätt: skriver ut bokstaven på rätt plats
+//   - Fel: Man blir av med 1 gissning, ritar ut en till del av gubben
+//          Och bokstaven skrivs upp
+// - Ny gissning
+//   Om inte det står en bokstav på alla streck
+//   Eller om man fått slut på gissningar
+
+string word = "banana";
+
+List<string> underscores = new();
+
+for (int i = 0; i < word.Length; i++)
+{
+  underscores.Add("_");
+}
+
+Console.WriteLine(string.Join(" ", underscores));
+
+Console.WriteLine("GISSA");
+
+string guess = "";
+
+while (guess.Length != 1)
+{
+  guess = Console.ReadLine();
+}
+
+Console.WriteLine($"You guessed {guess}!");
+
+Console.ReadLine();
